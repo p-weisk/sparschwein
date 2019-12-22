a82d7186c41d33295ad80606a47dabde7ff249dd
Paul Weiske <p.weisk@gmail.com>
2019-12-22 23:38:34 +0100

    Saschas komische nervige extrawümsche

--- client/sparschwein-client/src/components/BalanceWidget.js
+++ client/sparschwein-client/src/components/BalanceWidget.js
@@ -9,26 +9,36 @@ import {
 import Money from './Money';
 
 const BalanceWidget = (props) => {
-    return <Row>
-        <Col>
-            <Progress animated
-                color="success" 
-                value={ props.budget } 
-                max={props.budget > props.spent ? props.budget : props.spent} 
-            />
-            <Alert color="success">
-                Budget: <Money amount={ props.budget } currency="€" />
-            </Alert>
-            <Progress animated 
-                color="danger" 
-                value={ props.spent || props.budget / 500 || 1 } 
-                max={props.budget > props.spent ? props.budget : props.spent}    
-            />
-            <Alert color="danger">
-                Ausgegeben: <Money amount={ props.spent } currency="€" />
-            </Alert>
-        </Col>
-    </Row>
+    return <>
+        <Row>
+            <Col>
+                <Progress striped style={{marginBottom: "8px"}}
+                    color="success" 
+                    value={ props.budget } 
+                    max={props.budget > props.spent ? props.budget : props.spent} 
+                />
+                <Progress striped 
+                    color="danger" 
+                    value={ props.spent || props.budget / 500 || 1 } 
+                    max={props.budget > props.spent ? props.budget : props.spent}    
+                />
+            </Col>
+        </Row>
+    <br />
+        <Row>
+            <Col sm={6}>
+                <Alert color="success">
+                    Budget: <Money amount={ props.budget } currency="€" />
+                </Alert>
+            </Col>
+            <Col sm={6}>
+                <Alert color="danger">
+                    Ausgegeben: <Money amount={ props.spent } currency="€" />
+                </Alert>
+            </Col>
+        </Row>
+        <hr className="my-3" />
+    </>
 }
 
 export default BalanceWidget;
