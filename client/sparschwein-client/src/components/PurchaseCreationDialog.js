8829da78271a335909f0c483e08ee68514cd9c52
Paul Weiske <p.weisk@gmail.com>
2019-12-22 23:38:49 +0100

    Add purchase form

--- client/sparschwein-client/src/components/PurchaseCreationDialog.js
+++ client/sparschwein-client/src/components/PurchaseCreationDialog.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Alert, Col, Row, Spinner, Button } from 'reactstrap';
+
+import { Alert, Button, Col, Form, Input, Label, Row, Spinner, FormGroup } from 'reactstrap';
 import { Redirect } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRedoAlt } from '@fortawesome/free-solid-svg-icons';
 
@@ -21,57 +23,67 @@ class PurchaseCreationDialog extends Component {
     }
 
     render() {
+        const sendFunc = () => {
+            const body = JSON.stringify({
+                Title: this.state.Title,
+                User: this.state.User,
+                Timestamp: this.state.Timestamp.toISOString(),
+                Total: this.state.Total,
+                Comment: this.state.Comment,
+                Payment: this.state.Payment,
+            });
+            fetch("http://localhost:8000/api/purchases", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: body
+            }).then(() => {
+                this.props.updateBalance();
+                this.setState({loadingStatus: "redirect"});
+            }).catch(() => {
+                this.setState({loadingStatus: "error"});
+            });
+        }
         const feedback = this.state.loadingStatus === "error" ? <Alert color="danger">Fehler</Alert> : "";
         const redirectingButton = this.state.loadingStatus === "redirect" ?
             <Redirect to="/" />
             : <Button onClick={
                 () => {
-                    const body = JSON.stringify({
-                        Title: this.state.Title,
-                        User: this.state.User,
-                        Timestamp: this.state.Timestamp.toISOString(),
-                        Total: this.state.Total,
-                        Comment: this.state.Comment,
-                        Payment: this.state.Payment,
-                    });
                     this.setState({loadingStatus: "loading"});
-                    fetch("localhost:8000/api/purchases", {
-                        method: "POST",
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: body
-                    }).then(() => {
-                        this.props.updateBalance();
-                        this.setState({loadingStatus: "redirect"});
-                    }).catch(() => {
-                        this.setState({loadingStatus: "error"});
-                    });
+                    setTimeout(sendFunc, 500);
                 }
-            }>Send</Button>;
+            }>{this.state.loadingStatus === 'loading' ? <Spinner color="primary"/> : "Send"}</Button>;
 
         return <Row>
             <Col>
                 {
                     feedback
                 }
-                <form>
-                    <label>Titel</label>
-                    <input type="text" onChange={
+                <Form className="form">
+                <FormGroup>
+                    <Label>Titel</Label>
+                    <Input type="text" id="title" onChange={
                         (e) => {
                             this.setState({Title: e.target.value});
                         }
                     } key="Title" name="Title" />
-                    <br />
-                    <label>Einkäufer</label>
-                   <input type="text" onChange={
+                </FormGroup>
+                <FormGroup>
+                    <Label>Einkäufer</Label>
+                   <Input type="text" id="user" onChange={
                         (e) => {
                             this.setState({User: e.target.value});
                         }
                     } key="User" name="User" />
+                </FormGroup>
+                <FormGroup>
+                    <Label>Kaufdatum</Label>
                     <br />
-                    <label>Kaufdatum</label>
                     <DatePicker 
+                        className="form-control"
+                        allowSameDay={ true }
+                        dateFormat="dd.MM.yyyy"
                         selected={this.state.Timestamp} 
                         onChange={
                             (date) => {
@@ -79,32 +91,42 @@ class PurchaseCreationDialog extends Component {
                             }
                         }
                     />
-                    <br />
-                    <label>Gesamtpreis</label>
-                    <input type="text" onChange={
+                </FormGroup>
+                <FormGroup>
+                    <Label>Gesamtpreis</Label>
+                    <Input type="number" id="total" onChange={
                         (e) => {
                             this.setState({Total: Number.parseInt(e.target.value, 10)});
                         }
                     } key="Total" name="Total" />
-                    <br />
-                    <label>Kommentar</label>
-                    <input type="text" onChange={
+                </FormGroup>
+                <FormGroup>
+                    <Label>Kommentar</Label>
+                    <Input type="text" id="comment" onChange={
                         (e) => {
                             this.setState({Comment: e.target.value});
                         }
                     } key="Comment" name="Comment" />
-                    <br />
-                    <label>Zahlweise</label>
-                    <input type="text" onChange={
+                </FormGroup>
+                <FormGroup>
+                    <Label>Zahlweise</Label>
+                    <Input type="select" onChange={
                         (e) => {
                             this.setState({Payment: e.target.value});
                         }
-                    } key="Payment" name="Payment" />
-
-                </form>
+                    } key="Payment" name="Payment"
+                    >
+                        <option>Barzahlung</option>
+                        <option>Kartenzahlung</option>
+                    </Input>
+                </FormGroup>
+                <br />
+                <FormGroup>
                 {
                     redirectingButton
                 }
+                </FormGroup>
+                </Form>
             </Col>
         </Row>
     }
